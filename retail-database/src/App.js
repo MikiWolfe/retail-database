aeff6083cf5e641a63c4dd4146fd7d00b31574d0
Mikayla Bruce <mkla.w.b@gmail.com>
2022-05-11 14:56:40 -0700

    react app created

---
+++ retail-database/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+     
+    <h1> Welcome to the online internet database </h1>
+ 
+          <button> ENTER IF YOU DARE </button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
