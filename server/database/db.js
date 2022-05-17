7c039f5f2bc1e7e06886a6a5d8c2cba1fcd10a21
Mikayla Bruce <mkla.w.b@gmail.com>
2022-05-17 13:25:10 -0700

    changes

--- server/database/db.js
+++ server/database/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/retail_db', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/retailDatabase', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 
