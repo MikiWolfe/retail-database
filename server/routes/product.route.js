639eb1561294d6737a427580a1de00f0f5f88ca5
Mikayla Bruce <mkla.w.b@gmail.com>
2022-05-17 12:11:53 -0700

    more of it now works

--- server/routes/product.route.js
+++ server/routes/product.route.js
@@ -4,22 +4,20 @@ const router = express.Router();
 
 let productSchema = require("../models/Product");
 
-
 router.get("/", (req, res) => {
   productSchema.find((error, data) => {
- 
     if (error) {
-      return next(error);
+      return error;
     } else {
       res.json(data);
     }
   });
 });
 
-router.post("/create-product", (req, res, next) => {
+router.post("/create-product", (req, res) => {
   productSchema.create(req.body, (error, data) => {
     if (error) {
-      return next(error);
+      return error;
     } else {
       console.log(data);
       res.json(data);
@@ -27,39 +25,37 @@ router.post("/create-product", (req, res, next) => {
   });
 });
 
-router
-  .route("/update-product/:id")
-  .get((req, res) => {
-    productSchema.findById(req.params.id, (error, data) => {
+router.route("/update-product/:id").get((req, res, next) => {
+  productSchema.findById(req.params.id, (error, data) => {
+    if (error) {
+      return next(error);
+    } else {
+      res.status(200).json(data);
+    }
+  });
+});
+
+router.route("/update-product/:id").put((req, res, next) => {
+  productSchema.findByIdAndUpdate(
+    req.params.id,
+    {
+      $set: req.body,
+    },
+    (error, data) => {
       if (error) {
         return next(error);
       } else {
-        res.json(data);
+        res.status(200).json(data);
+        console.log("Product updated!");
       }
-    });
-  })
+    }
+  );
+});
 
-  .put((req, res, next) => {
-    productSchema.findByIdAndUpdate(
-      req.params.id,
-      {
-        $set: req.body,
-      },
-      (error, data) => {
-        if (error) {
-          return next(error);
-        } else {
-          res.json(data);
-          console.log("Product updated!");
-        }
-      }
-    );
-  });
-
-router.delete("/delete-product/:id", (req, res, next) => {
+router.delete("/delete-product/:id", (req, res) => {
   productSchema.findByIdAndRemove(req.params.id, (error, data) => {
     if (error) {
-      return next(error);
+      return error;
     } else {
       res.status(200).json({ msg: data });
     }
