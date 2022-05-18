2c59beb89d649195773e1f26692ed23fcb53f61d
Mikayla Bruce <mkla.w.b@gmail.com>
2022-05-18 01:46:29 -0700

    more code

--- client/src/component/AddProduct.js
+++ client/src/component/AddProduct.js
@@ -1,10 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import NavBar from "./bars/NavBar";
 import Header from "./Header";
 import ProductForm from "./ProductForm";
-import { useNavigate } from "react-router-dom";
-// import Button from "react-bootstrap/esm/Button";
+import Button from "react-bootstrap/esm/Button";
 
 const AddProduct = () => {
   const [formValues, setFormValues] = useState({
@@ -15,9 +14,7 @@ const AddProduct = () => {
     quantity: "",
   });
 
-  const navigate = useNavigate();
-
-  const onClick = (productObject) => {
+  const onSubmit = (productObject) => {
     axios
       .post(
         "https://retailer-database.herokuapp.com/product/create-product",
@@ -27,13 +24,11 @@ const AddProduct = () => {
         if (res.status === 200) {
           alert("Product successfully added!");
         } else Promise.reject();
+        
       })
       .catch((err) => console.log(err));
   };
-  const onSubmit = () => {
-    onClick();
-    navigate("/homepage");
-  };
+
   return (
     <div>
       <Header />
@@ -45,6 +40,10 @@ const AddProduct = () => {
       >
         Add Product
       </ProductForm>
+      <Button variant="primary" size="lg" block="block" type="submit">
+        {" "}
+        Add another product
+      </Button>
     </div>
   );
 };
--- client/src/component/EditProduct.js
+++ client/src/component/EditProduct.js
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from "react";
-import { useParams, useNavigate } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import axios from "axios";
 import NavBar from "./bars/NavBar";
-import Header from "./Header";
+import Header from './Header'
 import ProductForm from "./ProductForm";
 
 const EditProduct = (props) => {
   const { id } = useParams();
-
+  console.log(id)
+  console.log(props);
   const [formValues, setFormValues] = useState({
     name: "",
     brand: "",
@@ -15,47 +16,45 @@ const EditProduct = (props) => {
     price: "",
     quantity: "",
   });
-
-  const navigate  = useNavigate()
-
-
   const onSubmit = (productObject) => {
+    console.log(productObject);
     axios({
       method: "put",
-      url: `https://retailer-database.herokuapp.com/product/update-product/${id}`,
+      url: `http://localhost:4000/product/update-product/${id}`,
       data: productObject,
       headers: {
         "Content-Type": "application/json",
       },
     })
       .then((res) => {
+        console.log(res);
         if (res.status === 200) {
           alert("Product successfully updated");
+          console.log(res)
         } else Promise.reject();
       })
       .catch((err) => alert(err));
-    };
-    
-    useEffect(() => {
-      axios({
-        method: "get",
-        url: `https://retailer-database.herokuapp.com/product/update-product/${id}`,
-        headers: {
-          "Content-Type": "application/json",
-        },
-      })
+  };
+
+  useEffect(() => {
+    axios({
+      method: "get",
+      url: `http://localhost:4000/product/update-product/${id}`,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
       .then((res) => {
         const { name, brand, category, price, quantity } = res.data;
         setFormValues({ name, brand, category, price, quantity });
       })
       .catch((err) => console.log(err));
-      navigate('/homepage');
-    }, []);
-    
+  },);
+
   return (
     <div>
-      <Header />
-      <NavBar />
+       <Header/>
+      <NavBar/>
       <ProductForm
         initialValues={formValues}
         onSubmit={onSubmit}
