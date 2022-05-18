0efeed89b938b08b261d8c65eb2627688a71bda2
Mikayla Bruce <mkla.w.b@gmail.com>
2022-05-17 21:58:18 -0700

    forgot to define path

--- server/server.js
+++ server/server.js
@@ -1,13 +1,17 @@
+const path = require('path')
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
-let express = require('express');
-let cors = require('cors');
-let bodyParser = require('body-parser');
-let db = require('./database/db');
+const db = require('./database/db');
   
 // Express Route
 const productRoute = require('./routes/product.route')
-  
 const app = express();
+
+// PORT
+const PORT = process.env.PORT || 4000;  
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: false
@@ -21,13 +25,11 @@ if (process.env.NODE_ENV === "production") {
 app.get("*",(req,res) => { 
     res.sendFile(path.join(__dirname + "/client/build/index.html")) 
 })  
-// PORT
-const PORT = process.env.PORT || 4000;
+
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
-
 
 // 404 Error
 app.use((req, res, next) => {
@@ -39,3 +41,4 @@ app.use(function (err, req, res, next) {
   if (!err.statusCode) err.statusCode = 500;
   res.status(err.statusCode).send(err.message);
 });
+
