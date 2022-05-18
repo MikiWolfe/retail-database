057434caaf088bfbea7dc7522501f96054dafd12
Mikayla Bruce <mkla.w.b@gmail.com>
2022-05-18 00:39:50 -0700

    code

--- server/routes/product.route.js
+++ server/routes/product.route.js
@@ -14,57 +14,58 @@ router.get("/", (req, res) => {
   });
 });
 
-router
-  .route("/create-product")
-  .post((req, res) => {
-    productSchema.create(req.body, (error, data) => {
-      if (error) {
-        return error;
-      } else {
-        console.log(data);
-        res.json(data);
-      }
-    });
-  })
-  .get("/create-product", (req, res) => {
-    productSchema.findOne((error, data) => {
-      if (error) {
-        return error;
-      } else {
-        console.log(data);
-        res.json(data);
-      }
-    });
+router.post("/create-product", (req, res) => {
+  productSchema.create(req.body, (error, data) => {
+    if (error) {
+      return error;
+    } else {
+      console.log(data);
+      res.json(data);
+    }
   });
+});
 
-router
-  .route("/update-product/:id")
-  .get((req, res) => {
-    productSchema.findById(req.params.id, (error, data) => {
+router.get("/create-product", (req, res) => {
+  productSchema.findOne((error, data) => {
+    if (error) {
+      return error;
+    } else {
+      console.log(data);
+      res.json(data);
+    }
+  });
+});
+
+router.route("/update-product/:id")
+.get((req, res) => {
+  productSchema.findById(
+    req.params.id, (error, data) => {
+    if (error) {
+      return next(error);
+    } else {
+      res.json(data);
+    }
+  });
+})
+.put((req, res, next) => {
+  productSchema.findByIdAndUpdate(
+    req.params.id,
+    {
+      $set: req.body,
+    },
+    { new: true },
+    (error, data) => {
       if (error) {
         return next(error);
       } else {
-        res.json(data);
+        res.status(200).json(data);
+        console.log(data, "Product updated!");
       }
-    });
-  })
-  .put((req, res, next) => {
-    productSchema.findByIdAndUpdate(
-      req.params.id,
-      {
-        $set: req.body,
-      },
-      { new: true },
-      (error, data) => {
-        if (error) {
-          return next(error);
-        } else {
-          res.status(200).json(data);
-          console.log(data, "Product updated!");
-        }
-      }
-    );
-  });
+    }
+  );
+});
+
+
 
 router.delete("/delete-product/:id", (req, res) => {
   productSchema.findByIdAndRemove(req.params.id, (error, data) => {
