cffccb8ebeb1ff53a7b483e3559570e27eed788e
Mikayla Bruce <mkla.w.b@gmail.com>
2022-05-18 01:01:16 -0700

    code

--- client/src/component/ProductTableRow.js
+++ client/src/component/ProductTableRow.js
@@ -1,24 +1,30 @@
 import React from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
-import  Button  from "react-bootstrap/Button";
-
+import Button from "react-bootstrap/Button";
+import { useHistory } from "react-router-dom";
 
 const ProductTableRow = (props) => {
   const { _id, name, brand, category, price, quantity } = props.obj;
+  const history = useHistory();
 
   const deleteProduct = () => {
     axios
-      .delete(`https://retailer-database.herokuapp.com/product/delete-product/${_id}`)
+      .delete(
+        `https://retailer-database.herokuapp.com/product/delete-product/${_id}`
+      )
       .then((res) => {
         if (res.status === 200) {
           alert("Product successfully deleted");
-          window.location.reload();
         } else Promise.reject();
       })
       .catch((err) => alert("Something went wrong"));
+    window.location.reload();
   };
-
+  function onClick() {
+    deleteProduct();
+    history.push("/product");
+  }
   return (
     <tr>
       <td>{_id}</td>
@@ -29,10 +35,13 @@ const ProductTableRow = (props) => {
       <td>{quantity}</td>
       <td>
         <Link to={`/update-product/${_id}`}>
-          <Button variant="info" size="sm">Edit   </Button>
+          <Button variant="info" size="sm">
+            Edit{" "}
+          </Button>
         </Link>
-<p> OR </p>
-        <Button onClick={deleteProduct} size="sm" variant="danger">
+        <p> OR </p>
+
+        <Button onClick={onClick} size="sm" variant="danger">
           Delete
         </Button>
       </td>
