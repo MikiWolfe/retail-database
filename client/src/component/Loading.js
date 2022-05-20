a76cde0fe182460b4618daa0d02c83f7a0d906d2
Mikayla Bruce <mkla.w.b@gmail.com>
2022-05-19 23:08:45 -0700

    see if that fixes it

--- client/src/component/Loading.js
+++ client/src/component/Loading.js
@@ -1,37 +1,17 @@
 import React from "react";
 import Spinner from "react-bootstrap/Spinner";
 import Modal from "react-bootstrap/Modal";
-import Button from "react-bootstrap/Button";
 
 export default function Loading() {
   return (
-      <Modal size="lg" keyboard={false}>
     <Modal.Dialog>
       <Modal.Title>Thank you for your patience! </Modal.Title>
 
       <Modal.Body>
-      <Button variant="primary" disabled>
-    <Spinner
-      as="span"
-      animation="border"
-      size="sm"
-      role="status"
-      aria-hidden="true"
-    />
-    <span className="visually-hidden">Loading...</span>
-  </Button>{' '}
-  <Button variant="primary" disabled>
-    <Spinner
-      as="span"
-      animation="grow"
-      size="sm"
-      role="status"
-      aria-hidden="true"
-    />
-    Loading...
-  </Button>
+        <p>
+          Loading ... <Spinner animation="border" variant="success" />{" "}
+        </p>
       </Modal.Body>
     </Modal.Dialog>
- </Modal>
   );
 }
